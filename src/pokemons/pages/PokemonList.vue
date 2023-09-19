1094a7188a77dbb87d4292ed5e8f9b222f85de02
JordyV23 <jordyvargas0123@gmail.com>
2023-09-19 14:08:30 -0600

    Integracion de axios Tipado de respuestas HTTP

--- src/pokemons/pages/PokemonList.vue
+++ src/pokemons/pages/PokemonList.vue
@@ -1,8 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import pokemonApi from '../api/pokemonApi'
+import type { PokemonsListResponse } from '../interfaces/pokemons-list.response'
+
+pokemonApi.get<PokemonsListResponse>('/pokemon?limit=45').then((resp) => console.log(resp.data.results))
+</script>
 <template>
   <div>
     <h1>Pokemon List</h1>
   </div>
 </template>
-
-
