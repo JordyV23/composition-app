232888cd50a28b0d4a4b12db34cfae5b133cec5a
JordyV23 <jordyvargas0123@gmail.com>
2023-09-23 09:33:10 -0600

    Instalacion de tanstack

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 import App from './App.vue'
 import router from './router'
 
+
 const app = createApp(App)
+
+// app.use(VueQueryPlugin)
+// Con
+VueQueryPlugin.install(app, {
+    queryClientConfig: {
+        defaultOptions: {
+            queries: {
+                cacheTime: 1000 * 120, //Cada 2 minutos
+                refetchOnReconnect: 'always', //Cada vez que se reconecte
+            }
+        }
+    }
+})
 
 app.use(router)
 
