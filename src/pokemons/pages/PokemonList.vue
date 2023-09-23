977e9eeb9fb30bff7abbb3d64265e54f5d310ba5
JordyV23 <jordyvargas0123@gmail.com>
2023-09-23 09:49:42 -0600

    Implementacion de tanstack

--- src/pokemons/pages/PokemonList.vue
+++ src/pokemons/pages/PokemonList.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
-import { usePokemons } from '../composables/usePokemons'
+import { useQuery } from '@tanstack/vue-query'
+import { getPokemons } from '../helpers/get-pokemons'
 
-const { count, pokemons, isLoading } = usePokemons()
+// import { usePokemons } from '../composables/usePokemons'
+import { computed } from 'vue'
 
+// const { count, pokemons, isLoading } = usePokemons()
 
+const { isLoading, data: pokemons } = useQuery(['pokemons'], getPokemons)
 
+const count = computed(() => pokemons.value?.length ?? 0);
 </script>
 <template>
   <div>
